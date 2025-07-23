aaae485b2172225ffa711217c0a56932ce2681b5
KOUFU-DIY <2963736779@qq.com>
2025-07-24 00:53:47 +0800

    Update sh1108.c

--- Oled_Driver/sh1108.c
+++ Oled_Driver/sh1108.c
@@ -66,8 +66,8 @@ void SH1108_Set_Address_ypage(unsigned char page)
 void SH1108_Set_Address_x_ypage(unsigned char x,unsigned char page)
 {
 	//方式1:单字节发送
-	SH1108_Set_Address_x(x);
-	SH1108_Set_Address_ypage(page);
+	//SH1108_Set_Address_x(x);
+	//SH1108_Set_Address_ypage(page);
 	
 	//方式2:连续发送(优化等级开高了可能有问题)
 	uint8_t i[]={((x>>4) | 0x10),(x&0x0f),0xb0,page};
